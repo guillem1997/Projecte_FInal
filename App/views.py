c4e435615263cf1778cfb22e71c3b134fc490665
Aram <puigcapdevila.a@gmail.com>
2020-11-23 18:48:29 +0100

    dll 23/11

--- App/views.py
+++ App/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.shortcuts import redirect
 import psycopg2
 
 
@@ -8,7 +9,16 @@ def home_page_view(request):
     return render(request, 'Home.html')
 
 def receptes_view(request):
-    return render(request, 'Receptes.html')
+    conn = psycopg2.connect(dbname="projecte_final",
+                            user="postgres",
+                            password="patata")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM notas_guia;")
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    params = {'notas_guia': result}
+    return render(request, 'Receptes.html', params)
 
 def ingredients_view(request):
     return render(request, 'Ingredients.html')
@@ -39,7 +49,19 @@ def select(request):
 
 def anadir(request):
     conn = psycopg2.connect(dbname="projecte_final",
-                            user="aram",
+                            user="postgres",
                             password="patata")
-    return HttpResponse('insertado')
+
+    dificultat = request.POST["name_dificultat"]
+    titol = request.POST["nombre_titulo"]
+    nota = request.POST["name_nota"]
+
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO notas_guia VALUES ('{dificultat}','{titol}','{nota}');")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return redirect('http://127.0.0.1:8000/receptes')
+
 
