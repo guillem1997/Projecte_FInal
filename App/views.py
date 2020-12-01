d33089a325f633c53a6636d035a2fb42f8866c87
Guillem <guillemsimohospedales9@gmail.com>
2020-12-01 20:03:17 +0100

    01/12/2020

--- App/views.py
+++ App/views.py
@@ -23,40 +23,41 @@ def new_ingredient(request):
                             user="postgres",
                             password="patata")
 
-    name = request.POST["value_ingredient"]
-    kcal = request.POST["value_Kcal"]
-    carbohidrates = request.POST["value_Carbohidrates"]
-    proteina = request.POST["value_Proteina"]
-    grasas = request.POST["value_Grasas"]
-    fibra = request.POST["value_Fibra"]
-    omnivore = request.POST["value_Omnivore"]
-    vegetarian = request.POST["value_Vegetarian"]
-    vegan = request.POST["value_Vegan"]
-    milk = request.POST["value_Milk"]
-    eggs = request.POST["value_Eggs"]
-    fish = request.POST["value_Fish"]
-    crustaceans = request.POST["value_Crustaceans"]
-    nuts = request.POST["value_Nuts"]
-    peanut = request.POST["value_Peanuts"]
-    gluten = request.POST["value_Gluten"]
-    soy = request.POST["value_Soy"]
-    january = request.POST["value_January"]
-    february = request.POST["value_February"]
-    mars = request.POST["value_Mars"]
-    april = request.POST["value_April"]
-    may = request.POST["value_May"]
-    jun = request.POST["value_Jun"]
-    july = request.POST["value_July"]
-    august = request.POST["value_August"]
-    september = request.POST["value_September"]
-    october = request.POST["value_October"]
-    november = request.POST["value_November"]
-    december = request.POST["value_December"]
+    name            =   request.POST["value_ingredient"]
+    kcal            =   request.POST["value_Kcal"]
+    carbohidrates   =   request.POST["value_Carbohidrates"]
+    proteina        =   request.POST["value_Proteina"]
+    grasas          =   request.POST["value_Grasas"]
+    fibra           =   request.POST["value_Fibra"]
+    omnivore        =   request.POST["value_Omnivore"]
+    vegetarian      =   request.POST["value_Vegetarian"]
+    vegan           =   request.POST["value_Vegan"]
+    milk            =   request.POST["value_Milk"]
+    eggs            =   request.POST["value_Eggs"]
+    fish            =   request.POST["value_Fish"]
+    crustaceans     =   request.POST["value_Crustaceans"]
+    nuts            =   request.POST["value_Nuts"]
+    peanut          =   request.POST["value_Peanuts"]
+    gluten          =   request.POST["value_Gluten"]
+    soy             =   request.POST["value_Soy"]
+    january         =   request.POST["value_January"]
+    february        =   request.POST["value_February"]
+    mars            =   request.POST["value_Mars"]
+    april           =   request.POST["value_April"]
+    may             =   request.POST["value_May"]
+    jun             =   request.POST["value_Jun"]
+    july            =   request.POST["value_July"]
+    august          =   request.POST["value_August"]
+    september       =   request.POST["value_September"]
+    october         =   request.POST["value_October"]
+    november        =   request.POST["value_November"]
+    december        =   request.POST["value_December"]
 
     insert = (f"INSERT INTO ingredients VALUES ('{name}','{kcal}','{carbohidrates}','{proteina}','{grasas}',"
               f"'{fibra}','{omnivore}','{vegetarian}','{vegan}','{milk}','{eggs}','{fish}','{crustaceans}',"
               f"'{nuts}','{peanut}','{gluten}','{soy}','{january}','{february}','{mars}','{april}','{may}',"
               f"'{jun}','{july}','{august}','{september}','{october}','{november}','{december}');")
+
     insert = insert.replace("''", "null")
     cursor = conn.cursor()
     cursor.execute(insert)
@@ -92,16 +93,16 @@ def new_recipe(request):
                             password="patata")
 
 
-    title = request.POST["value_title"]
-    time = request.POST["value_time"]
-    difficulty = request.POST["value_difficulty"]
-    description = request.POST["value_description"]
+    title       =   request.POST["value_title"]
+    time        =   request.POST["value_time"]
+    difficulty  =   request.POST["value_difficulty"]
+    description =   request.POST["value_description"]
 
     cursor = conn.cursor()
     cursor.execute("SELECT nextval('plats_id_seq');")
     contador = cursor.fetchall()
     contador = contador[0][0]
-    cursor.execute("INSERT INTO plats VALUES (%s, %s, %s, %s, %s)", (contador,title, time, difficulty, description))
+    cursor.execute("INSERT INTO plats VALUES (%s, %s, %s, %s, %s)", (contador, title, time, difficulty, description))
     conn.commit()
     cursor.close()
     conn.close()
